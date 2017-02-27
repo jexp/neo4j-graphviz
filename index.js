ac69349f0e859f185620fbe4a1b02e14709dc61c
Michael Hunger <github@jexp.de>
2017-02-27 02:07:37 +0100

    Improved graphviz output
    * record shape
    * draw rels behind nodes
    * color-scheme by label
    * semi-transparent rels
    * curved rels
    * Helvetica font
    * Smaller K

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ echo 'MATCH path = (a)-[r]->(b) RETURN path LIMIT 10' | node index.js '****' [fi
 
 var neo4j = require('neo4j-driver').v1;
 var graphviz = require('graphviz');
+// colors from: http://flatuicolors.com/
+var colors = {all:["#2ecc71","#1abc9c","#3498db","#9b59b6","#34495e","#16a085","#f1c40f","#e67e22",
+                   "#e74c3c","#95a5a6","#f39c12","#2980b9","#8e44ad","#27ae60","#2c3e50","#bdc3c7",
+                   "#c0392b","#d35400"], 
+              used:{}};
 
 function merge(o1,o2) {
 	for(var k in o2) {
@@ -24,15 +29,18 @@ function merge(o1,o2) {
 function getId(field) {
 	return field.constructor.name == "Integer" ? field.toString() : (field.identity) ? field.identity.toString() : null;
 }
+function labels(node) {
+	return ":"+node.labels.join(":");
+}
 function name(node) {
-	var x = ["^name$","^title$","^label$","name$","title$","label$"];
+	var x = ["^name$","^title$","^label$","value","name$","title$","label$",""];
     var props = node.properties;
 	for (var i=0;i<x.length;i++) {
 		for (k in props) {
 			if (props.hasOwnProperty(k) && k.toLowerCase().match(x[i])) return props[k];
 		}
 	}
-	return node.labels.join(" ");
+	return node.identity.toString();
 }
 
 function addGraphData(digraph, data, field) {
@@ -43,9 +51,18 @@ function addGraphData(digraph, data, field) {
         if (type == "Integer") return field.toString();
    		if (type == "Node") {
 				if (!(id in data.nodes)) {
+				var nLabels = labels(field);
+				var color = colors.used[nLabels] || colors.all.pop() || ["white","black"];
+				if (!(nLabels in colors.used)) {
+					colors.used[nLabels] = color;
+				}
 				data.nodes[id]=field;
-				var n = digraph.addNode(id, {label:name(field)}); // merge({lblString:field.labels},field.properties));
+				var n = digraph.addNode(id, {label:nLabels + "|" + name(field)}); // merge({lblString:field.labels},field.properties));
 				n.set( "style", "filled" );
+				n.set( "shape", "Mrecord" );
+				n.set( "fillcolor", color );
+				n.set( "fontcolor", "white" );
+				n.set("fontname","Helvetica");
 				return n;
 			}
 		}
@@ -54,7 +71,8 @@ function addGraphData(digraph, data, field) {
 				data.rels[id]=field;
 //				console.log("addEdge",getId(field.start), getId(field.end))
 				var e = digraph.addEdge(getId(field.start), getId(field.end),{label:field.type}); // , merge({type:field["type"]}, field.properties));
-				e.set( "color", "black" );
+				e.set( "color", "#00 00 00 40" );
+				e.set("fontname","Helvetica");
 				return e;
 			}
 		}
@@ -106,6 +124,13 @@ function renderGraph(query) {
 	    // Create digraph G
 	    var g = graphviz.digraph("G");
 	    g.set("overlap",false);
+	    g.set("concentrate",true);
+	    g.set("K",0.2);
+	    g.set("outputorder","edgesfirst");
+	    g.set("splines","curved");
+	    g.set("rankdir","LR");
+	    g.set("dpi",50);
+	    g.set("fontname","Helvetica");
 	    result.records.forEach(function(record) {
 	      addRecord(g, data, record);
 	    });
